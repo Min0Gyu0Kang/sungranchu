122b8188485b5b77a2dd286752c948211b5387e9
donggundg <rlaehdrjs98@g.skku.edu>
2024-11-13 18:50:57 +0900

    [Feat] 백엔드 프로젝트 생성

---
+++ Back/src/test/java/__2/SWE3002_42/Team/_4/sungranchu/SungranchuApplicationTests.java
@@ -0,0 +1,13 @@
+package __2.SWE3002_42.Team._4.sungranchu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SungranchuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
