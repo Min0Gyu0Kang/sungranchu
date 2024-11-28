fb90c5daafb17d6322572c673271f3880599397c
HypePenguin <gasidori@naver.com>
2024-11-29 07:07:09 +0900

    add input modal component

--- Front/src/pages/profile/modifyProfile.jsx
+++ Front/src/pages/profile/modifyProfile.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./profile.css";
 import ItemButton from "../../component/itemButton/ItemButton";
 import UpperNav from "../../component/upperNav/UpperNav";
+import InputModal from "../../component/InputModal/InputModal";
 
 import baseImage from './img/basic_profile.png';
 import profileIcon from './img/profile_icon.png'
@@ -11,10 +12,16 @@ export default function ModifyPage () {
   const goBack = true;
   const [profileImage, setProfileImage] = useState(baseImage);
   const [nickname, setNickname] = useState("마라엽떡"); // 닉네임은 api를 이용해서 불러와야 함.
+  const [modalOpen, setModalOpen] = useState(false);
+
+  function handleModifyClick () {
+    setModalOpen(true);
+  }
 
   return (
     <div className="container-achievement">
       <UpperNav title="개인정보 수정" goBack={goBack} />
+      <InputModal isOpen={modalOpen} setModalOpen={setModalOpen} question="닉네임을 입력하세요." />
 
       {/* Upper Area */}
       <div className="modify-upper-area">
@@ -30,7 +37,7 @@ export default function ModifyPage () {
       </div>
 
       <div className="modify-list-container">
-        <ItemButton imageSrc={profileIcon} title="닉네임 수정하기" subtitle="나를 나타내는 예쁜 닉네임을 지어봐요."/>
+        <ItemButton imageSrc={profileIcon} title="닉네임 수정하기" subtitle="나를 나타내는 예쁜 닉네임을 지어봐요." onClick={handleModifyClick}/>
       </div>
     </div>
   );
