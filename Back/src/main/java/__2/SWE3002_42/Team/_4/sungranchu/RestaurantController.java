51a761df66eb6ab662d93313ddc60f77180c2015
donggundg <136912097+donggundg@users.noreply.github.com>
2024-11-30 05:13:55 +0900

    Update RestaurantController.java

--- Back/src/main/java/__2/SWE3002_42/Team/_4/sungranchu/RestaurantController.java
+++ Back/src/main/java/__2/SWE3002_42/Team/_4/sungranchu/RestaurantController.java
@@ -26,4 +26,15 @@ public class RestaurantController {
         restaurantRepository.save(restaurant);
         return "redirect:/rrr";
     }
+
+    @GetMapping("/searchRestaurant/{name}")
+    public ResponseEntity<List<Restaurant>> searchRestaurant(@PathVariable String name) {
+        List<Restaurant> restaurant = restaurantRepository.findByName(name);
+        if(restaurant.size() != 0){
+            System.out.println("찾음!" + restaurant);
+            return ResponseEntity.ok(restaurant);
+        }
+        System.out.println("못찾음");
+        return ResponseEntity.notFound().build();
+    }
 }
